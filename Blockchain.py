708aa75915be466f6402f7c3ba0ff1706adce358
xyzhrey <xyzhrey@gmail.com>
2026-04-20 16:53:55 +0000

    main code complete, web remains

--- Blockchain.py
+++ Blockchain.py
@@ -151,10 +151,7 @@ class Blockchain:
         return True
 
     def resolve_conflicts(self):
-        """
-        Resolve conflicts between blockchain's nodes
-        by replacing our chain with the longest one in the network.
-        """
+        
         neighbours = self.nodes
         new_chain = None
 
@@ -183,3 +180,127 @@ class Blockchain:
         return False
 
 
+app =Flask(__name__)
+CORS(app)
+
+blockchain=Blockchain()
+
+
+@app.route('/')
+def index():
+    return render_template('./template/index.html')
+
+
+@app.route('/configure')
+def configure():
+    return render_template('./template/configure.html')
+
+
+
+
+@app.route('/transaction/new', methods=['POST'])
+def new_transaction():
+    values =request.form
+
+    required = ['sender_address', 'recipient_address', 'amount', 'signature']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    transaction_result=blockchain.submit_transaction(values['sender_address'], values['recipient_address'], values['amount'], values['signature'])
+
+    if transaction_result==False:
+        response = {'message': 'Invalid Transaction'}
+        return jsonify(response), 201
+    
+    else:
+        response={'message', 'Transaction will be added to block'}
+        return jsonify(response), 201
+    
+
+@app.route('/transactions/get', method=['GET'])
+def get_transactions():
+    transactions=blockchain.transactions
+
+    response={'transactions': transactions}
+    return jsonify(response), 200
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain':blockchain.chain,
+        'length':len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block=blockchain.chain[-1]
+    nonce = blockchain.proof_of_work()
+
+
+    blockchain.submit_transaction(sender_address=MINING_SENDER, recipient_address=blockchain.node_id, value=MINING_REWARD, signature='')
+    previous_hash=blockchain.hash(last_block)
+    block=blockchain.create_block(nonce, previous_hash)
+
+    response={
+        'message': 'new block forged',
+        "block_number":block['block_number'],
+        'transactions':block['transactions'],
+        'nonce':block[nonce],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    values = request.form
+    nodes = values.get('nodes').replace(" ", "").split(',')
+
+    if nodes is None:
+        return "Error: Please supply a valid list of nodes", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': 'New nodes have been added',
+        'total_nodes': [node for node in blockchain.nodes],
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'Our chain was replaced',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/get', methods=['GET'])
+def get_nodes():
+    nodes = list(blockchain.nodes)
+    response = {'nodes': nodes}
+    return jsonify(response), 200
+
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port)
